b4111c7fe63f70a069376507a489b154b691ce04
Dineth <dinethfiverr23@gmail.com>
2025-03-27 10:55:49 +0530

    Create View And Listing Out Food Items

--- CCApp/views.py
+++ CCApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Food
 # Create your views here.
+
+
+def index(request):
+    """View function for home page of site."""
+
+    foods = Food.objects.all()
+
+
+    return render(request, 'ccapp/index.html', {'foods': foods})
