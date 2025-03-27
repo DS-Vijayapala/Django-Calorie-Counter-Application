087384628139ad596e2af05ae02202770637eeaf
Dineth <dinethfiverr23@gmail.com>
2025-03-27 11:20:13 +0530

    Adding Consume Model

--- CCApp/models.py
+++ CCApp/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Food(models.Model):
+    """ Model representing a food item. """
 
     name = models.CharField(max_length=500)
     carbs = models.FloatField()
@@ -13,3 +15,10 @@ class Food(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Consume(models.Model):
+    """ Model representing a food item consumed by a user. """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    food_consumed = models.ForeignKey(Food, on_delete=models.CASCADE)
