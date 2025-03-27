7e67be13355bbb63b5353f3f1ae1f82aa50acd97
Dineth <dinethfiverr23@gmail.com>
2025-03-27 12:04:10 +0530

    Add User Functionality To Add Food And Listing Consumed Food Items

--- CCApp/views.py
+++ CCApp/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Food , Consume
 # Create your views here.
 
 
 def index(request):
     """View function for home page of site."""
 
-    foods = Food.objects.all()
+    if request.method == 'POST':
+
+        user = request.user
+
+        food_consumed = request.POST['food_consumed']
+
+        consume = Food.objects.get(name=food_consumed)
+
+        user = request.user
+
+        consume = Consume(user=user, food_consumed=consume)
+
+        consume.save()
+
+        foods = Food.objects.all()
+
+    else:
+
+        foods = Food.objects.all()
+
+    consumed_food = Consume.objects.filter(user=request.user)
+
+    context = {'foods': foods, 'consumed_food': consumed_food}
 
 
-    return render(request, 'ccapp/index.html', {'foods': foods})
+    return render(request, 'ccapp/index.html', context)
